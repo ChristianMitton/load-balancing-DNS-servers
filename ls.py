5ea496bd00fbc2afd6db0e0e688b2e52fedf2ae5
Abhishek Patel <abhi.p@rutgers.edu>
2020-03-26 18:08:53 -0400

    threading

--- ls.py
+++ ls.py
@@ -1,6 +1,8 @@
 import socket
 import sys
 import time
+import Queue
+from threading import Thread, Event
 
 #* .AF_INET corresponds to IPV4
 #* SOCK_STREAM corresponds to TCP
@@ -19,27 +21,27 @@ def convertFileToArray(file_path):
     result = []
 
     contents = f.readlines()
-    for line in contents:        
+    for line in contents:
         line = line.rstrip('\n')
         line = line.rstrip('\r')
 
         # split line on space
-        splitString = line.split()        
+        splitString = line.split()
 
         result.append(splitString)
-    
+
     f.close()
-    return result    
+    return result
 
 def getDomainFromTable(dns_table, client_domain):
-    for row in dns_table:        
+    for row in dns_table:
         hostname = row[0]
         ip_address = row[1]
         flag = row[2]
 
         if(client_domain == hostname):
             entry = ' '.join(row)
-            return entry       
+            return entry
 
     return False
 
@@ -47,14 +49,36 @@ def getDomainFromTable(dns_table, client_domain):
 #     for row in dns_table:
 #         flag = row[2]
 #         if(flag == 'NS'):
-#             entry = ' '.join(row)            
+#             entry = ' '.join(row)
 #             # print('entry: ' + entry)
 #             return entry
 
+def send_to_ts(stop_thread, socket, client_domain, host_name, listen_port):
+    try:
+        ts = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # print("[C]: Client socket created")
+    except ts1.error as err:
+        print('{} \n'.format("socket open error ",err))
 
-def server():    
+    connection = (socket.gethostbyname(host_name), listen_port)
+    ts.connect(connection)
+    ts.send(client_domain.encode('utf-8'))
 
-    listen_port = int(sys.argv[1])    
+    start_time = time.time()
+    while time.time() - start_time < 6:
+        ts_server_response = ts.recv(100)
+        ts_msg = ts_server_response.decode('utf-8')
+        if len(ts_msg) > 0:
+            print "Here"
+            global result
+            result = ts_msg
+            stop_thread.set()
+            return
+
+
+def server():
+
+    listen_port = int(sys.argv[1])
 
     ts1_host_name = sys.argv[2] + ""
 
@@ -62,7 +86,7 @@ def server():
 
     ts2_host_name = sys.argv[4] + ""
 
-    ts2_listen_port = int(sys.argv[5])    
+    ts2_listen_port = int(sys.argv[5])
 
     #? create a socket
     try:
@@ -71,92 +95,76 @@ def server():
     except s.error as err:
         print('{} \n'.format("socket open error ",err))
 
-    #? bind the socket to an ip address and port number, using tuples: s.bind(<ip_address>, <port_number>)      
-    #! Node: socket.gethostname is the ip address of whatever machine server is running on. It doesn't change      
-    s.bind((socket.gethostname(), listen_port))    
+    #? bind the socket to an ip address and port number, using tuples: s.bind(<ip_address>, <port_number>)
+    #! Node: socket.gethostname is the ip address of whatever machine server is running on. It doesn't change
+    s.bind((socket.gethostname(), listen_port))
 
     host=socket.gethostname()
     print("[LS]: Server host name is: " + host)
-    
+
     ip_address=(socket.gethostbyname(host))
     print("[LS]: Server IP address is " + ip_address)
 
     s.listen(1)
 
     #? Server will listen forever
-    while True:        
+    while True:
+
+        global result
+        result = None
+
         print("[LS]: Server is listening...")
 
-        #? If a connection is heard, the clientSocket object and ip_address from the connection are unpacked into 
+        #? If a connection is heard, the clientSocket object and ip_address from the connection are unpacked into
         #? the 'clientsocket' and 'address' variables
-        clientsocket, address = s.accept() 
+        clientsocket, address = s.accept()
         print "[LS]: Got a connection request from a client at", address
 
         #? recieving domain given by client
         client_domain = clientsocket.recv(100).decode('utf-8')
-        print "\n[LS]: Domain recieved from client: " + client_domain        
+        print "\n[LS]: Domain recieved from client: " + client_domain
 
         # serverMsg = "I recieved your domain (" + client_domain + ")"
         # print "sending (" + serverMsg + ") back to client.\n"
         # clientsocket.send(serverMsg.encode('utf-8'))
 
-        #TODO listen for 5 seconds
-        for i in range(5):
-            #? send domain to ts1 server
-            try:
-                ts1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                # print("[C]: Client socket created")
-            except ts1.error as err:
-                print('{} \n'.format("socket open error ",err)) 
+        any_thread_done = Event()
 
-            connection = (socket.gethostbyname(ts1_host_name), ts1_listen_port)
-            ts1.connect(connection) 
-            ts1.send(client_domain.encode('utf-8'))
+        ts1 = Thread(target=send_to_ts, args=(any_thread_done, socket, client_domain, ts1_host_name, ts1_listen_port))
+        ts2 = Thread(target=send_to_ts, args=(any_thread_done, socket, client_domain, ts2_host_name, ts2_listen_port))
 
-            ts1_server_response = ts1.recv(100)
-            ts1_msg = ts1_server_response.decode('utf-8')        
+        ts1.start()
+        ts2.start()
 
-            #? send domain to ts2 server
-            try:
-                ts2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                # print("[C]: Client socket created")
-            except ts2.error as err:
-                print('{} \n'.format("socket open error ",err)) 
+        start_time = time.time()
+        while time.time() - start_time < 5 and not any_thread_done.is_set():
+            time.sleep(0.1)
 
-            connection = (socket.gethostbyname(ts2_host_name), ts2_listen_port)
-            ts2.connect(connection) 
-            ts2.send(client_domain.encode('utf-8'))
+        global result
 
-            ts2_server_response = ts2.recv(100)
-            ts2_msg = ts2_server_response.decode('utf-8')
+        if result is None:
+            print "Error"
 
-            if ts1_msg == "" or ts2_msg == "":
-                #TODO interrupt timmer, send which ever message is not empty                
-                # break            
-                pass
-            time.sleep(1)
-
-        if ts1_msg != "":
-            clientsocket.send(ts1_msg.encode('utf-8'))
-        elif ts2_msg != "":
-            clientsocket.send(ts2_msg.encode('utf-8'))
-        else:
+        if result is None:
             error_msg = client_domain + " - ERROR: HOST NOT FOUND"
             clientsocket.send(error_msg.encode('utf-8'))
+        else:
+            clientsocket.send(result.encode('utf-8'))
+
 
         # search for domain in dns_table
         # dns_ipaddress = getDomainFromTable(dns_table, client_domain)
 
-        # if(dns_ipaddress == False):                        
+        # if(dns_ipaddress == False):
         #     serverMsg =  getTSHostName(dns_table)
         #     print "[LS]: Could not find domain. Sending '" + serverMsg + "' back to client\n"
         #     clientsocket.send(serverMsg.encode('utf-8'))
-        # else:            
+        # else:
         #     serverMsg = dns_ipaddress
         #     print "[LS]: Found domain.\n> Sending '" + serverMsg + "' back to client\n"
         #     clientsocket.send(serverMsg.encode('utf-8'))
 
-        # clientsocket.close()       
+        # clientsocket.close()
 
     s.close()
     exit()
--- ts1.py
+++ ts1.py
@@ -11,20 +11,20 @@ def convertFileToArray(file_path):
 
     contents = f.readlines()
 
-    for line in contents:        
+    for line in contents:
         line = line.rstrip('\n')
         line = line.rstrip('\r')
 
         # split current line in file on space character
-        splitString = line.split()        
+        splitString = line.split()
 
         result.append(splitString)
 
-    f.close() 
-    return result    
+    f.close()
+    return result
 
 def getDomainFromTable(dns_table, server_domain):
-    for row in dns_table:        
+    for row in dns_table:
         hostname = row[0]
         ip_address = row[1]
         flag = row[2]
@@ -38,7 +38,7 @@ def getDomainFromTable(dns_table, server_domain):
 def server():
     file_path = "PROJ2-DNSTS1.txt"
 
-    listen_port = int(sys.argv[1])    
+    listen_port = int(sys.argv[1])
 
     dns_table = convertFileToArray(file_path)
 
@@ -49,12 +49,12 @@ def server():
     except s.error as err:
         print('{} \n'.format("socket open error ",err))
 
-    #? bind the socket to an ip address and port number, using tuples: s.bind(<ip_address>, <port_number>)    
-    s.bind((socket.gethostname(), listen_port))    
-    
+    #? bind the socket to an ip address and port number, using tuples: s.bind(<ip_address>, <port_number>)
+    s.bind((socket.gethostname(), listen_port))
+
     host = socket.gethostname()
     print("[TS1]: TS1 Server host name is: " + host)
-    
+
     ip_address = (socket.gethostbyname(host))
     print("[TS1]: TS1 Server IP address is " + ip_address)
 
@@ -65,31 +65,31 @@ def server():
     while True:
         print("[TS1]: TS1 Server is listening...")
 
-        #? If a connection is heard, the ls_socket object and ip_address from the connection are unpacked into 
+        #? If a connection is heard, the ls_socket object and ip_address from the connection are unpacked into
         #? the 'ls_socket' and 'address' variables
-        ls_socket, address = s.accept() 
-        print "[TS1]: Got a connection request from the server at", address        
+        ls_socket, address = s.accept()
+        print "[TS1]: Got a connection request from the server at", address
 
         #? recieving word given by server
         server_domain = ls_socket.recv(100).decode('utf-8')
-        print "\n[TS1]: Domain recieved from server: " + server_domain        
+        print "\n[TS1]: Domain recieved from server: " + server_domain
 
         #? search for domain in dns_table
         dns_ipaddress = getDomainFromTable(dns_table, server_domain)
 
-        if(dns_ipaddress == False):            
+        if(dns_ipaddress == False):
             #? If DNS hasn't been found, do nothing/send nothing back to LS
             print "[TS1]: Could not find domain '" + server_domain + "'\n"
             pass
-            # serverMsg = server_domain + ' - Error:HOST NOT FOUND'                        
+            # serverMsg = server_domain + ' - Error:HOST NOT FOUND'
             # print "[TS1]: Could not find domain. Sending '" + serverMsg + "' back to server\n"
             # ls_socket.send(serverMsg.encode('utf-8'))
         else:
             serverMsg = dns_ipaddress
             print "[TS1]: Found domain.\n> Sending '" + serverMsg + "' back to LS server\n"
-            ls_socket.send(serverMsg.encode('utf-8'))        
+            ls_socket.send(serverMsg.encode('utf-8'))
 
-        ls_socket.close()       
+        ls_socket.close()
 
     s.close()
     exit()
